5a0bcf65a437a3cc2ef4ce16993636cc9b791801
Ivy Cobbs <128192555+2026iec1@users.noreply.github.com>
2023-04-24 17:52:32 -0400

    Update game.py

--- game.py
+++ game.py
@@ -1,5 +1,24 @@
 # Ivy Cobbs - rkg5jw
-# Emma Conrod -
+# Emma Conrod - eup2fs
+
+# Description: The user is a "good guy" in a jungle fighting a team of ninjas, which fly down from the trees
+# and also drop bombs from above that must be avoided. You can hit the ninjas--which awards points--by moving
+# the sword across the bottom of the screen. If you hit a bomb, you lose a life. There are also stars that fall along
+# with the ninjas and bombs, and collecting a star awards one additional life.
+
+# 3 basic features: user input, game over, and graphics
+#   user input: the user can move the sword by pressing the left and right arrow keys.
+#   game over: when you hit so many bombs that you run out of lives, the game ends.
+#   graphics: we use images from the web, saved locally, to create gameboxes for the sword, ninjas, bombs, health bar,
+#   stars, and jungle background.
+
+# 4 additional features: restart from game over, enemies, collectables, and health bar
+#   restart from game over: when the game ends, the user has the option to restart gameplay by pressing the space bar
+#   enemies: the falling ninjas are the enemies that the user should "hit" in order to gain points
+#   collectables: the stars that fall down add one life to the user's health bar when "collected"
+#   health bar: the health bar appears at the top of the screen in the form of red hearts, one for each "life" the
+#   player has left. When the player gets a star, an additional heart appears, when they hit a ninja, one heart
+#   disappears, and when there are no hearts left, the game ends.
 
 import uvage
 
@@ -9,24 +28,23 @@ camera = uvage.Camera(800, 600)
 lives_left = 3
 game_over = False
 points = 0
-clock = 0  # I haven't written code for this in the function yet but I was thinking we could use it to tell the game 
+clock = 0  # I haven't written code for this in the function yet, but I was thinking we could use it to tell the game
 # which baddie to spawn at a given moment. Like every x amount of time, we can cycle through the same pattern of baddies
 # falling from the top
 
-# setting up all the gameboxes and scaling them to the proper size
-# Emma: all of these images are saved in the repository :)
+# setting up all the gameboxes and scaling them to the proper size. All of these images are saved in the repository :)
 background = uvage.from_image(400, 300, "jungle_background.jpeg")
 background.size = [800, 600]
 sword = uvage.from_image(400, 560, "sword.png")
 sword.scale_by(0.15)
 sword.speedx = 0
 sword.speedy = 0
-baddie1 = uvage.from_image(100, -30, "baddie.png")
-baddie1.scale_by(0.2)
-baddie2 = uvage.from_image(150, -30, "baddie.png")
-baddie2.scale_by(0.2)
-baddie3 = uvage.from_image(200, -30, "baddie.png")
-baddie3.scale_by(0.2)
+ninja1 = uvage.from_image(100, -30, "baddie.png")
+ninja1.scale_by(0.2)
+ninja2 = uvage.from_image(150, -30, "baddie.png")
+ninja2.scale_by(0.2)
+ninja3 = uvage.from_image(200, -30, "baddie.png")
+ninja3.scale_by(0.2)
 heart1 = uvage.from_image(30, 30, "heart.png")
 heart1.size = [40, 40]
 heart2 = uvage.from_image(70, 30, "heart.png")
@@ -44,10 +62,10 @@ def tick():
     camera.draw(background)
     clock += 1
     if not game_over:
-        baddie1.y += 5
-    if baddie1.bottom_touches(sword, 0, -10):
+        ninja1.y += 5
+    if ninja1.bottom_touches(sword, 0, -10):
         points += 1
-        baddie1.y = -30
+        ninja1.y = -30
     if uvage.is_pressing("left arrow"):
         sword.move_speed()
         if sword.speedx >= -10:
@@ -67,7 +85,7 @@ def tick():
     if lives_left == 0:
         game_over = True
     camera.draw(score)
-    camera.draw(baddie1)
+    camera.draw(ninja1)
     camera.draw(sword)
     camera.display()
 
